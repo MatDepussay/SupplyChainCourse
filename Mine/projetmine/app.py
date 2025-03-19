f4488121193bdaada4499ba2c7055b5d95250a9d
Mathias Depussay <mathias.depussay@etu.univ-tours.fr>
2025-03-19 17:28:14 +0100

    modified:   Mine/projetmine/__pycache__/__init__.cpython-313.pyc 	new file:   Mine/projetmine/__pycache__/app.cpython-313.pyc 	modified:   Mine/projetmine/__pycache__/data.cpython-313.pyc 	modified:   Mine/projetmine/app.py

--- Mine/projetmine/app.py
+++ Mine/projetmine/app.py
@@ -1,55 +1,65 @@
 from projetmine import reseau, data
-def run():
-    data.load
 
-    for gare in ListeGares:
-        gare.loadLignes()
-        gare.loadVoisins()
+def run():
+    data.load_entreprises()
 
     Menu = 0
     while(True):
         if(Menu == 0):
             print("Que voulez-vous faire ?")
-            print("1 - Afficher la carte")
-            print("2 - Trouver un trajet")
-            print("3 - Quitter")
+            print("1 - Afficher la liste des entreprises")
+            print("2 - Ajouter une entreprise")
+            print("3 - Modifier une entreprise")
+            print("4 - Calculer la demande maximale")
+            print("5 - Quitter")
 
             result = int(input())
 
-            if(1<= result <= 3):
+            if(1<= result <= 5):
                 Menu = result
             else:
                 print("Input invalide")
-        elif(Menu == 3):
+        elif(Menu == 4):
             break
         elif(Menu == 1):
-            affich.afficherCarte()
+            for entreprise in reseau.ListeEntreprises:
+                print(entreprise)
             Menu=0
         elif(Menu == 2):
-            print("Rentrez le nom de la gare de départ :")
-
-            gareDepart = None
-            while( not gareDepart):
-                result = str(input())
-                for gare in ListeGares:
-                    if(gare.nom == result):
-                        gareDepart = gare
-                if( not gareDepart):
-                    print("La gare ", result, " n'existe pas ! Réessayez : ")
-
-            print("Rentrez le nom de la gare d'arrivée :")
-            gareArrivee = None
-            while( not gareArrivee):
-                result = str(input())
-                for gare in ListeGares:
-                    if(gare.nom == result):
-                        gareArrivee = gare
-                if( not gareArrivee):
-                    print("La gare ", result, " n'existe pas ! Réessayez : ")
-
-            Trajet = gareDepart.cheminOptimalVers(gareArrivee)
-            print("Le trajet passera par les gares suivantes : ")
-            for gare in Trajet[0]:
-                print(gare.nom)
-            print("Le trajet prendra ", Trajet[1], " minutes.")
+            print("Renrez le nom de l'entreprise")
+            result = ""
+            NouvelleEntreprise = reseau.Entreprise(result)
+            resultatValide = False
+            while(not resultatValide):
+                if(result ==""):
+                    print("Entrez un nom d'au moins 1 caractere! \n")
+                    resultatValide = False
+                elif(NouvelleEntreprise in reseau.ListeEntreprise):
+                    print("Ce nom d'entreprise existe déjà! \n")
+                    resultatValide = False
+                else:
+                    resultatValide = True
+            resultatValide = False
+            while(not resultatValide):
+                print("Enrtez une transofrmation effectué par l'entreprise : \n")
+                print("Matière 1 : ")
+                result1 = str(input())
+                print("Matière 2 : ")
+                result2 = str(input())
+                print("Capacité de transformation : ")
+                result3 = int(input())
+                NouvelleEntreprise.ajouterTransfo([(result1, result2), result3])
+                print("Voulez-vous ajouter une autre transformation ? (O/N)")
+                result4 =2
+                while not(0<=result4<=1):
+                    result4= int(input())
+                    if(not (0<=result4<=1)):
+                        print("Input invalide")
+                if(result4 == 0):
+                    resultatValide = True
+                else:
+                    resultatValide = False
+    
+            reseau.ListeEntreprise.append(NouvelleEntreprise)
+            data.save()
             Menu = 0
