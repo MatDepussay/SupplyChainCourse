bd678c3b5dc4d9fc5b98f3601745e9c6f9e8bc3e
Theo Girard <girard-theo@orange.fr>
2025-03-26 14:49:15 +0100

    projet gateau V1

--- projetgateau/src/projetgateau/recettes.py
+++ projetgateau/src/projetgateau/recettes.py
@@ -1,11 +1,17 @@
 
 from typing import List
+from graphlib import *
 
 class sousProduit(object):
 
     def __init__(self, nom:str):
         self.nom :str = nom
 
+    def __eq__(self, autre):
+        return self.nom == autre.nom
+
+    def __hash__(self):
+        return hash(self.nom)
 
 
 class etape(object):
@@ -19,3 +25,76 @@ class etape(object):
         self.active: bool = etapeActive
         self.sousProduitFinal: sousProduit = produitFinal
         self.sousProduitNecessaires : List[sousProduit] = sousProduitNecessaires
+
+    def __eq__(self, autre):
+        if self.sousProduitFinal == autre.sousProduitFinal and self.sousProduitNecessaires == autre.sousProduitNecessaires:
+            return True
+        return False
+
+
+class recette(object):
+    def __init__(self, listeEtapes):
+        self.listeEtapes:List[etape] = listeEtapes
+
+        self.listeSousProduits:List[sousProduit] = []
+        for step in self.listeEtapes:
+            if step.sousProduitFinal not in self.listeSousProduits:
+                self.listeSousProduits.append(step.sousProduitFinal)
+
+    def verifierValidite(self) -> bool:
+        """
+        Vérifie si les sous-produits necessaires pour la recette peuvent être obtenu via une étape de la recette.
+
+        Paramètres :
+        - Aucun
+
+        Renvoie :
+        - booléen : Vrai si la recette est auto-suffisante, faux sinon.
+        """
+        for step in self.listeEtapes:
+            for prerequis in step.sousProduitNecessaires:
+                if prerequis not in self.listeSousProduits:
+                    return False
+        return True
+
+
+class planning(object):
+
+    def __init__(self, listeRecette):
+        self.listeRecette:List[recette] = listeRecette
+
+        self.listeSousProduits = []
+        for recipe in self.listeRecette:
+            self.listeSousProduits += [ prod for prod in recipe.listeSousProduits if prod not in self.listeSousProduits]
+
+        self.listeEtapes = []
+        for recipe in self.listeRecette:
+            self.listeEtapes += [ step for step in recipe.listeEtapes if step not in self.listeEtapes]
+
+        self.planning : List[etape] = []
+
+    def genererPlanning(self):
+
+        graph = dict()
+        for sousProduit in self.listeSousProduits:
+            L = []
+            for step in self.listeEtapes:
+                if(step.sousProduitFinal == sousProduit):
+                    L += [ produits for produits in step.sousProduitNecessaires if produits not in L]
+            graph[sousProduit] = list(L) #<---
+        ts = TopologicalSorter(graph)
+        triTopo = list(ts.static_order())
+        self.planning = []
+        for prod in triTopo:
+            for step in self.listeEtapes:
+                if step.sousProduitFinal == prod:
+                    self.planning.append(step)
+
+    def __str__(self):
+        S = "Ordre possible : \n"
+        temps = 0
+        for i, step in enumerate(self.planning):
+            S+= str(i+1) + ") "+ step.nom + ", duree : "+ str(step.duree) + " min\n"
+            temps += step.duree
+        S+= "Temps total : "+str(temps)+" min."
+        return S
